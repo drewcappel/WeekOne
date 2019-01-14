a0aacf8f5a9445290ca302500c39f61d1c0c97ea
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-01-14 18:49:26 -0500

    Added label, button function, and print statements.

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("😎😎 viewDidLoad() just executed!")
     }
 
-
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+        print("I just pressed the button!")
+    }
+    
 }
 
